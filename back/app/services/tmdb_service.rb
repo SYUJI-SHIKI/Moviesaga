ad7e374dd736fc9f3c2dba427992d3ceaf120a67
SYUJI-SHIKI <syuzi00777@gmail.com>
2024-05-23 16:17:52 +0900

    APIの機能

--- back/app/services/tmdb_service.rb
+++ back/app/services/tmdb_service.rb
@@ -28,25 +28,28 @@ class TmdbService
   end
 
   def self.get_random_movie_title
-    popular_movie_ids = get_popular_movies
-    return nil if popular_movie_ids.nil? || popular_movie_ids.empty?
+    begin
+      popular_movie_ids = get_popular_movies
+      return nil if popular_movie_ids.nil? || popular_movie_ids.empty?
 
-    random_movie_id = popular_movie_ids.sample
-    response = HTTParty.get("#{BASE_URL}/movie/#{random_movie_id}", query: {
-      api_key: API_KEY,
-      language: "ja",
-    })
-
-    if response.success?
-      movie = JSON.parse(response.body)
-      {
-        title: movie['title'],
-        overview: movie['overview'],
-        postpath: movie['poster_path'],
-      }
-    else
-      Rails.logger.error("Failed to fetch TMDB data: #{response.message}")
-      nil
+      random_movie_id = popular_movie_ids.sample
+      response = HTTParty.get("#{BASE_URL}/movie/#{random_movie_id}", query: {
+        api_key: API_KEY,
+        language: "ja",
+      })
+      if response.nil?
+        Rails.logger.error("APIからのレスポンスがnilです")
+        return { title: nil, overview: nil, postpath: nil }
+      end
+      if response.success?
+        return { title: response[:title], overview: response[:overview], postpath: response[:postpath] }
+      else
+        Rails.logger.error("API呼び出しに失敗しました: #{response.error_message}")
+        return { title: nil, overview: nil, postpath: nil }
+      end
+    rescue StandardError => e
+      Rails.logger.error("API呼び出し中にエラーが発生しました: #{e.message}")
+      return { title: nil, overview: nil, postpath: nil }
     end
   end
 end
