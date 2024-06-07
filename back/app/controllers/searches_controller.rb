f9edeb2e2f3a6b020aea10aded8b9220575e3983
SYUJI-SHIKI <syuzi00777@gmail.com>
2024-06-07 22:11:59 +0900

    エラー修正

--- back/app/controllers/searches_controller.rb
+++ back/app/controllers/searches_controller.rb
@@ -5,11 +5,11 @@ class SearchesController < ApplicationController
 
   def search
     if params[:query].present? && params[:category].present?
-      search = fetch_tmdb_search(params[:query], params[:category])
+      search_results = fetch_tmdb_search(params[:query], params[:category])
       if params[:category] == "movie"
-        movie_ids = search["results"].map { |movie| { id: movie["id"], poster_path: movie["poster_path"] } }
+        movie_ids = search_results.map { |movie| { id: movie["id"], poster_path: movie["poster_path"] } }
       elsif params[:category] == "person"
-        person_ids = search["results"].map { |person| person["id"] }
+        person_ids = search_results.map { |person| person["id"] }
         movie_ids = person_ids.flat_map do |person_id|
           credits = fetch_person_movie_credits(person_id)
           credits["cast"].map { |movie| { id: movie["id"], poster_path: movie["poster_path"] } }
@@ -18,28 +18,40 @@ class SearchesController < ApplicationController
       @movies = Kaminari.paginate_array(movie_ids).page(params[:page]).per(20)
       Rails.logger.debug("Movies: #{movie_ids}")
     else
-      @movies = Kaminari.paginate_array([]).page(params[:page]).per(20)  # 空の配列にもページネーションを追加
+      @movies = Kaminari.paginate_array([]).page(params[:page]).per(20) # 空の配列にもページネーションを追加
     end
   end
-
+  
   private
-
+  
   BASE_URL = "https://api.themoviedb.org/3"
   API_KEY = Rails.application.credentials.api_key[:tmdb]
-
+  
   def fetch_tmdb_search(query, category)
     endpoint = case category
-    when "movie" then "search/movie"
-    when "person" then "search/person"
+               when "movie" then "search/movie"
+               when "person" then "search/person"
+               end
+    max_pages = 5
+    page = 1
+    results = []
+  
+    loop do
+      response = HTTParty.get("#{BASE_URL}/#{endpoint}", query: {
+        api_key: API_KEY,
+        language: 'ja',
+        query: query,
+        page: page,
+      })
+  
+      parsed_response = JSON.parse(response.body)
+      results.concat(parsed_response["results"]) if parsed_response["results"]
+  
+      page += 1
+      break if page > max_pages
     end
-
-    response = HTTParty.get("#{BASE_URL}/#{endpoint}", query: {
-      api_key: API_KEY,
-      language: 'ja',
-      query: query,
-    })
-
-    JSON.parse(response.body)
+  
+    results
   end
 
   def fetch_person_movie_credits(person_id)
