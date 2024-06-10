3fcd28060da2d551e776526d948d16e8de5d3c41
SYUJI-SHIKI <syuzi00777@gmail.com>
2024-06-10 17:41:26 +0900

    frontを移行するためのテスト

--- back/config/application.rb
+++ back/config/application.rb
@@ -31,5 +31,6 @@ module App
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.active_job.queue_adapter = :solid_queue
   end
 end
--- back/config/routes.rb
+++ back/config/routes.rb
@@ -18,15 +18,39 @@ Rails.application.routes.draw do
   get 'search', to: 'searches#search'
   resources :favorites, only: %i[create destroy]
   resources :bookmarks, only: %i[create destroy]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+
+  namespace :api, format: 'json' do
+    namespace :v1 do
+      devise_for :users, controllers: { registrations: 'users/registrations' }
+      resource :profile, only: %i[show edit update]
+      resolve('Profile') { [:profile] }
+      resources :movies, only: %i[index show] do
+        collection do
+          get 'random'
+          get 'favorite'
+        end
+      end
+      resources :collections do
+        collection do
+          get 'my_lists'
+          get 'bookmark'
+        end
+      end
+      get 'search', to: 'searches#search'
+      resources :favorites, only: %i[create destroy]
+      resources :bookmarks, only: %i[create destroy]
+    
+      get "up" => "rails/health#show", as: :rails_health_check
+    
+      if Rails.env.development?
+        mount LetterOpenerWeb::Engine, at: "/letter_opener"
+      end
+    end
   end
 end
