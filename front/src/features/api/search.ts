a6958effbd5a699011fdbe61f87773bc5427a4c0
SYUJI-SHIKI <syuzi00777@gmail.com>
2024-07-04 07:05:50 +0900

    検索機能

--- front/src/features/api/search.ts
+++ front/src/features/api/search.ts
@@ -2,22 +2,16 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 const searchHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    // 型定義を明示的に行う
     const { query, category, page } = req.query as { query: string; category: string; page: string };
-    
-    // 環境変数のチェック
     const apiUrl = process.env.NEXT_PUBLIC_TEST_API_URL;
     if (!apiUrl) {
       throw new Error('NEXT_PUBLIC_TEST_API_URL is not defined');
     }
 
     const response = await fetch(`${apiUrl}/api/v1/search?query=${query}&category=${category}&page=${page}`);
-    
-    // エラーハンドリング
     if (!response.ok) {
       throw new Error(`Error fetching data: ${response.statusText}`);
     }
-
     const data = await response.json();
     res.status(200).json(data);
   } catch (error) {
--- front/src/pages/search.tsx
+++ front/src/pages/search.tsx
@@ -1,20 +1,15 @@
-"use client";
-import { useCallback, useState, useEffect, useRef } from 'react';
-import Image from 'next/image'
-import Link from 'next/link';
-import React from 'react';
-import styles from  '@/features/search/search.module.css';
-import { useRouter } from 'next/router';
+import React, { useCallback, useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import SearchForm from "@/components/elements/Search/searchForm";
+import MovieIndex from "@/components/elements/Movie/MovieIndex";
+import Pagination from "@/components/elements/Pagination/Pagination";
+import api from "lib/api";
 
 interface Movie {
   id: number;
+  original_title: string;
   poster_path: string;
 }
-interface Styles {
-  readonly [key: string]: string;
-}
-
-const cssModuleStyles: Styles = styles;
 
 const SearchPage: React.FC = () => {
   const router = useRouter();
@@ -23,104 +18,51 @@ const SearchPage: React.FC = () => {
   const [movies, setMovies] = useState<Movie[]>([]);
   const [page, setPage] = useState<number>(1);
   const [totalPages, setTotalPages] = useState<number>(1);
-  const imagesRef = useRef<HTMLDivElement[]>([]);
 
   const handleSearch = useCallback(async () => {
-    const response = await fetch(`/api/v1/search?query=${query}&category=${category}&page=${page}`);
-    const data = await response.json();
+    const response = await api(`/search?query=${query}&category=${category}&page=${page}`)
+    const data = await response.data;
     setMovies(data.movies);
     setTotalPages(data.total_pages);
     const queryString = `?query=${query}&category=${category}&page=${page}`;
-    router.push(`/search/search${queryString}`, undefined, { shallow: true });
-  }, [query, category, page, router]);
-
-  const handlePageChange = (newPage: number) => {
-    setPage(newPage);
-    handleSearch();
-  };
+    router.push(`/search${queryString}`, undefined, { shallow: true });
+  }, [query, category, page]);
 
   useEffect(() => {
     const { query: queryParam, category: categoryParam, page: pageParam } = router.query;
-    if (!queryParam && !categoryParam && !pageParam) return;
     if (queryParam) setQuery(queryParam as string);
     if (categoryParam) setCategory(categoryParam as string);
     if (pageParam) setPage(Number(pageParam));
-    handleSearch();
-  }, [router.query, handleSearch]);
-
-  useEffect(() => {
-    const currentImages = imagesRef.current;
-
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add(styles['animation-slide-in']);
-          observer.unobserve(entry.target);
-        }
-      });
-    }, { threshold: 0.1 });
-
-    currentImages.forEach((image) => {
-      observer.observe(image);
-    });
-
-    return () => {
-      currentImages.forEach((image) => {
-        observer.unobserve(image);
-      });
-    };
+    if (queryParam || categoryParam || pageParam) handleSearch();
   }, []);
 
+  // useEffect(() => {
+  //   if (query || category || page) {
+  //     handleSearch();
+  //   }
+  // }, [query, category, page, handleSearch]);
+
   return (
-    <>
-      <div className='min-h-screen bg-black flex flex-col justify-center'>
-        <div className='container mx-auto p-4 bg-black'>
-          <div className='flex justify-center m-4'>
-            <input
-              type="text"
-              value={query}
-              onChange={(e) => setQuery(e.target.value)}
-              placeholder="Search..."
-              className='p-2 border rounded-md w-full max-w-xs'
-            />
-            <select value={category} onChange={(e) => setCategory(e.target.value)}
-              className='ml-2 p-2 border rounded-md'>
-              <option value="movie">Movie</option>
-              <option value="person">Person</option>
-            </select>
-            <button onClick={handleSearch} className='ml-2 p-2 bg-blue-500 text-white rounded-md'>Search</button>
-          </div>
+    <div className="min-h-screen bg-black flex flex-col justify-center">
+      <div className="container mx-auto p-4 bg-black">
+        <SearchForm
+          query={query}
+          category={category}
+          onQueryChange={setQuery}
+          onCategoryChange={setCategory}
+          onSearch={handleSearch}
+        />
+        <MovieIndex movies={movies} />
+        <Pagination
+          currentPage={page}
+          totalPages={totalPages}
+          onPageChange={(newPage) => {
+            setPage(newPage);
 
-          <div className="flex flex-wrap justify-center p-4 ">
-            {movies.map((movie) => (
-              <Link key={movie.id} href={`/movies/${movie.id}`} passHref className=' border-2 border-gray-600/50 '>
-                <div className="block group cursor-pointer m-5">
-                  <div className={`relative overflow-hidden rounded-lg shadow-lg transform transition duration-300 group-hover:scale-105 ${cssModuleStyles['animate-slide-in']}`}>
-                    <Image
-                      src={movie.poster_path}
-                      className="w-full h-auto object-cover "
-                      width={100}
-                      height={150}
-                      alt={'movie.id'}
-                    />
-                  </div>
-                </div>
-              </Link>
-            ))}
-          </div>
-
-          <div className='flex justify-center mt-4'>
-            {Array.from({ length: totalPages }, (_, index) => (
-              <button key={index} onClick={() => handlePageChange(index + 1)}
-                className={`px-4 py-2 mx-1 rounded-md ${
-                  page === index + 1 ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}>
-                {index + 1}
-              </button>
-            ))}
-          </div>
-        </div>
+          }}
+        />
       </div>
-    </>
+    </div>
   );
 };
 
